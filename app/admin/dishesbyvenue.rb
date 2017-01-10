3053a639fd4a21a66bd39aa9c401c25b70b6816b
Raghu Betina <raghu@firstdraft.com>
2017-01-10 02:51:31 +0000

    Whitelist columns in Dishesbyvenue for ActiveAdmin

--- app/admin/dishesbyvenue.rb
+++ app/admin/dishesbyvenue.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Dishesbyvenue do
 
+ permit_params :venueid, :dishid
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
